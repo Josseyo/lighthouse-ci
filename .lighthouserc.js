3d063c6f14313f5a21e06389f37c057f1be129a3
Josseyo <159940877+Josseyo@users.noreply.github.com>
2025-06-04 08:27:09 +0000

    Setup Lighthouse CI with GitHub Actions

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -1,45 +1,20 @@
-/**
- * @license Copyright 2019 Google Inc. All Rights Reserved.
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
- * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
- */
-'use strict';
-
 module.exports = {
   ci: {
+    collect: {
+      numberOfRuns: 3,
+      url: [
+        'https://ltu-i0015n-2024-web.azurewebsites.net/'
+      ],
+    },
     assert: {
-      preset: 'lighthouse:recommended',
       assertions: {
-        'dom-size': ['error', {maxNumericValue: 3000}],
-
-        'csp-xss': 'off',
-        'unsized-images': 'off',
-        'uses-rel-preload': 'off',
-        'uses-responsive-images': 'off',
-        'uses-rel-preconnect': 'off',
-        'preload-lcp-image': 'off',
-        'offscreen-images': 'off',
-        'unused-javascript': 'off',
-
-        label: 'off',
-        'color-contrast': 'off',
-        bypass: 'off',
-        'tap-targets': 'off',
-
-        'apple-touch-icon': 'off',
-        'maskable-icon': 'off',
-        'installable-manifest': 'off',
-        'service-worker': 'off',
-        'splash-screen': 'off',
-        'themed-omnibox': 'off',
+        'categories:performance': ['warn', { minScore: 0.9 }],
+        'categories:accessibility': ['warn', { minScore: 0.9 }],
+        'categories:seo': ['warn', { minScore: 0.9 }],
       },
     },
     upload: {
-      urlReplacementPatterns: [
-        's/[0-9a-f]{12}$/HASH/',
-        's#:[0-9]{3,5}/#:PORT/#',
-        's/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/UUID/ig',
-      ],
+      target: 'temporary-public-storage',
     },
   },
 };
